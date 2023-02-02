c61c1ab0220eb1ab59f2833ff1c9820a52770e6f
anzelozturk <73177712+anzelozturk@users.noreply.github.com>
2023-02-02 12:51:45 +0300

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-admin.site.register(RegisteredUser)
-admin.site.register(Note)
-admin.site.register(NoteImages)
-admin.site.register(user_type)
-admin.site.register(Comment)
